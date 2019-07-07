ce15cbf22f6f31e0b5220d8ae2ee12ad1b14dbc2
lukasd2 <beowfull@gmail.com>
2019-07-08 01:16:14 +0200

    added descending ordering on frequencies, stub for heaps law

--- src/it/unipi/gestione/corpora/utils/ReadFromFiles.java
+++ src/it/unipi/gestione/corpora/utils/ReadFromFiles.java
@@ -6,8 +6,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class ReadFromFiles {
@@ -36,7 +40,7 @@ public class ReadFromFiles {
 		}
 	}
 	
-	private static class MutableInteger {
+	private static class MutableInteger implements Comparable<Integer> {
 	    int count = 1;
 	    
 	    public MutableInteger(int count) {
@@ -50,6 +54,13 @@ public class ReadFromFiles {
 	    public int getInteger() {
 	    	return this.count;
 	    }
+
+		@Override
+		public int compareTo(Integer value) {
+			if(value > this.count) return -1;
+			else if(value < this.count) return 1;
+			else return 0;
+		}
 	}
 
 
@@ -75,7 +86,8 @@ public class ReadFromFiles {
 				reader.readLine();
 			}
 			
-			int countWord = 0; 
+			int countWord = 0;
+			int threshold = 1000;
 			while (((currentLine = reader.readLine()) != null)) {
 				if(currentLine.equals("")) continue;
 				
@@ -86,6 +98,7 @@ public class ReadFromFiles {
 				for(String s : wordList) {
 					map.compute(s, (k, v) -> v == null
 							? new MutableInteger(0) : v).increment();
+					countWord += 1;
 					/*if (map.containsKey(s)) {
 			            // increment occurrence
 			            int occurrence = map.get(s);
@@ -97,6 +110,7 @@ public class ReadFromFiles {
 			        }*/
 				}
 				//System.out.println(currentLine);
+				if(countWord >= threshold) threshold = heapsLaw(threshold);
 			}
 			//System.out.println(countWord);
 
@@ -105,6 +119,19 @@ public class ReadFromFiles {
 		printResults();
 	}
 	
+	private int heapsLaw(int threshold) {
+		int k = 50;
+		double beta = Math.sqrt(threshold); // 0,5
+		System.out.println("beta" + beta);
+		
+		double value = k * Math.pow((double)threshold, beta);
+		
+		System.out.println("heaps: " + value);
+		
+		return threshold += 1000;
+		
+	}
+
 	private void printResults() throws IOException {
 			BufferedWriter writer = new BufferedWriter(new FileWriter("tokenizeResult.txt"));
 			map.entrySet().forEach(entry->{
@@ -120,17 +147,27 @@ public class ReadFromFiles {
 			 sortbykey();
 	}
 	
-	public void sortbykey() 
+	public void sortbykey() throws IOException 
     { 
 		// TODO need to write a custom comparator
-		
-        // TreeMap to store values of HashMap 
-        TreeMap<String, MutableInteger> sorted = new TreeMap<>(map); 
-  
-        // Display the TreeMap which is naturally sorted 
-        for (Map.Entry<String, MutableInteger> entry : sorted.entrySet())  
-            System.out.println("Key = " + entry.getValue().getInteger() +  
-                         ", Value = " + entry.getKey());
+		BufferedWriter writer2 = new BufferedWriter(new FileWriter("tokenizeResult2.txt"));
+
+		LinkedList<Entry<String, MutableInteger>> list = new LinkedList<>(map.entrySet());
+
+		Collections.sort(list, new Comparator<Map.Entry<String, MutableInteger>>()
+		  {
+		     public int compare(Map.Entry<String, MutableInteger> o1,
+		     Map.Entry<String, MutableInteger> o2) {
+		         return (o2.getValue()).compareTo(o1.getValue().getInteger());
+		       }
+
+		  });
+		for(Entry<String, MutableInteger> entry : list) {
+			writer2.write(entry.getKey() + " " + entry.getValue().getInteger());
+	        writer2.newLine();
+		}
+		writer2.close();
+		//System.out.println("The most common first name is: " + list.get(0).getKey() + " and it occurs: " + list.get(0).getValue().getInteger() + " times.");
     } 
 
 }
